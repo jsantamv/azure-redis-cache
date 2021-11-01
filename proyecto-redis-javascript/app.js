aad5d9a54446bdd6d5589563a630b11847c9f5bc
juan.santamaria <jsantamv@live.com>
2021-11-01 14:45:30 -0600

    Creacion del ejemplo en JavaScript

---
+++ proyecto-redis-javascript/app.js
@@ -0,0 +1,35 @@
+const Promise = require("bluebird");
+const redis = require("redis");
+require("dotenv").config();
+
+
+Promise.promisifyAll(redis);
+
+const client = redis.createClient(
+    process.env.REDISPORT,
+    process.env.REDISHOSTNAME,
+    {
+        password: process.env.REDISKEY,
+        tls: { servername: process.env.REDISHOSTNAME }
+    }
+);
+
+const CallRedisTest = async () => {
+
+    console.log("Adding value to the cache");
+    await client.setAsync("mi_llave", "esta es mi llave");
+
+    console.log("Reading value back:");
+    console.log(await client.getAsync("mi_llave"));
+
+    console.log("Pinging the cache");
+    console.log(await client.pingAsync());
+
+    //Elimine todas las claves de la caché con flushdbAsync
+    await client.flushdbAsync();
+
+    //Por último, cierre la conexión con quitAsync
+    await client.quitAsync();
+}
+
+CallRedisTest();
---
+++ proyecto-redis-javascript/readme.md
@@ -0,0 +1,16 @@
+# Inicializar el programa
+ mkdir redisapp
+ cd redisapp
+ npm init -y
+ touch app.js
+
+# Libreria para trabajar con JS
+
+[Doc](https://docs.microsoft.com/es-es/learn/modules/optimize-your-web-apps-with-redis/6-exercise-connect-an-app-to-the-cache?pivots=javascript)
+
+npm install redis bluebird dotenv
+
+- redis: el paquete de JavaScript que se usa con más frecuencia para conectarse a Redis.
+- bluebird: se usa para convertir los métodos de estilo de devolución de llamada del paquete redis en promesas que se pueden esperar.
+- dotenv: carga variables de entorno desde un archivo .env, que es donde se almacenará la información de conectividad de Redis.
+
